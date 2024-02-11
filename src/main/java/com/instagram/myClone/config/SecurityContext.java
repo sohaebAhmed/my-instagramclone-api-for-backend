1df7783729b1621d852c938af3c6cdb69fef61bc
Sohaeb Ahmed <sohaebahmed@gmail.com>
2024-02-11 18:19:51 +0530

    Update SecurityContext.java

--- src/main/java/com/instagram/myClone/config/SecurityContext.java
+++ src/main/java/com/instagram/myClone/config/SecurityContext.java
@@ -2,6 +2,6 @@ package com.instagram.myClone.config;
 
 public class SecurityContext {
 
-	public static final String JWT_KEY = "jfdhgkdfghdkfjhdjfh";
+	public static final String JWT_KEY = "jfdahdgjhaskashdlasjdlasdjifjdfjhgkdfghdkfjhdjfh";
 	public static final String HEADER = "Authorization";
 }
